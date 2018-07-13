79f39ef5101ae8384a5d3af31018cc52cd78f5f2
Andrés Felipe Cabeza <andresfelipecabeza16@gmail.com>
2018-07-13 01:15:42 -0500

    Update Logged.java

--- MailApplication/src/mailapplication/Logged.java
+++ MailApplication/src/mailapplication/Logged.java
@@ -19,7 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
  *
- * @author 
+ * @author Felipe Cabeza @felipecabeza16
  */
 public class Logged extends JFrame implements ActionListener{
     UsersConstructor searcher;
